96eaab274b16e646dc37d4e0d7bfc6fb969dc4f5
Dmitry Eremin <mitya1705@gmail.com>
2023-08-02 21:09:58 +0300

    Shared connection

--- src/test/java/ru/inno/ext/CompanyRepositoryResolver.java
+++ src/test/java/ru/inno/ext/CompanyRepositoryResolver.java
@@ -6,10 +6,9 @@ import ru.inno.db.CompanyRepositoryJdbc;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.SQLException;
 
-public class CompanyRepositoryResolver implements ParameterResolver{
-    Connection connection = null;
+public class CompanyRepositoryResolver implements ParameterResolver, BeforeAllCallback, AfterAllCallback {
+    private Connection connection = null;
 
     @Override
     public boolean supportsParameter(ParameterContext parameterContext, ExtensionContext extensionContext) throws ParameterResolutionException {
@@ -18,17 +17,24 @@ public class CompanyRepositoryResolver implements ParameterResolver{
 
     @Override
     public Object resolveParameter(ParameterContext parameterContext, ExtensionContext extensionContext) {
+        return new CompanyRepositoryJdbc(connection);
+    }
+
+    @Override
+    public void afterAll(ExtensionContext extensionContext) throws Exception {
+        System.out.println("disconnecting");
+        if (connection != null && !connection.isClosed()) {
+            connection.close();
+        }
+    }
+
+    @Override
+    public void beforeAll(ExtensionContext extensionContext) throws Exception {
         System.out.println("connecting");
         // TODO: define in .properties
         String connectionString = "jdbc:postgresql://dpg-chdkl0ak728nnn00sqv0-a.frankfurt-postgres.render.com/x_clients_db_yjdt";
         String user = "x_clients_user";
         String pass = "2hdwfMCel2i7SyZeOghoUVVOOwnpyfEL";
-        try {
-            connection = DriverManager.getConnection(connectionString, user, pass);
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-        return new CompanyRepositoryJdbc(connection);
+        connection = DriverManager.getConnection(connectionString, user, pass);
     }
-
 }
