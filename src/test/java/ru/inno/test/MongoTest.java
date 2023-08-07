09dfbd142cac3834a9442d9fe459e2aa54bac2f4
Dmitry Eremin <mitya1705@gmail.com>
2023-08-07 20:46:05 +0300

    mongo test init

---
+++ src/test/java/ru/inno/test/MongoTest.java
@@ -0,0 +1,6 @@
+package ru.inno.test;
+
+public class MongoTest {
+    public static final String CONNECTION_STRING = "mongodb://localhost:27017/?readPreference=primary&ssl=false&directConnection=true";
+
+}
